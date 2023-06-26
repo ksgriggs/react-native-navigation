e66ddd5b1a4d1f61c8c31c27da81377e38306372
Scott Griggs <sgriggs@gmail.com>
2023-06-26 17:40:28 -0400

    Add favorite meals functionality using Context API

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect } from 'react';
 import { Text, View, Image, StyleSheet, ScrollView } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
@@ -6,14 +6,23 @@ import MealDetails from '../components/MealDetails';
 import Subtitle from '../components/MealDetail/Subtitle';
 import List from '../components/MealDetail/List';
 import IconButton from '../components/IconButton';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 function MealsDetailScreen({ route, navigation }) {
+  const favoriteMealsCx = useContext(FavoritesContext);
+
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log('PRESSED');
+  const mealIsFavorite = favoriteMealsCx.ids.includes(mealId);
+
+  function changeFavoriteStatusHandler() {
+    if (mealIsFavorite) {
+      favoriteMealsCx.removeFavorite(mealId);
+    } else {
+      favoriteMealsCx.addFavorite(mealId);
+    }
   }
 
   useLayoutEffect(() => {
@@ -21,14 +30,14 @@ function MealsDetailScreen({ route, navigation }) {
       headerRight: () => {
         return (
           <IconButton
-            icon='star'
-            color='white'
-            onPress={headerButtonPressHandler}
+            icon={mealIsFavorite ? 'star' : 'star-outline'}
+            color="white"
+            onPress={changeFavoriteStatusHandler}
           />
         );
       },
     });
-  }, [navigation, headerButtonPressHandler]);
+  }, [navigation, changeFavoriteStatusHandler]);
 
   return (
     <ScrollView style={styles.rootContainer}>
